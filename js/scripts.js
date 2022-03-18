be40263c97762c22b1f149deafc696ca41412abc
Annette Warimwe <annettewarimwe@gmail.com>
2022-03-18 20:38:03 +0300

    Add a function to hide the delivery location if the user prefers to pick up the order
    Add variables to get the user's order form input
    
    Log the user's input for debugging purposes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,48 @@
+// Business Logic
+
+function order(size, price, crust, toppings, delivery) {
+  this.size = size;
+  this.price = price;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.delivery = delivery;
+}
+
+function crust(name, price) {}
+
+// if (order.size==='large') {
+//   order.price=
+// }
+
+// UI Logic
 $(document).ready(function () {
+  $("#loc").hide();
+  $("#delivery").click(function () {
+    if ($(this).is(":checked")) {
+      $("#loc").show(300);
+    } else {
+      $("#loc").hide(200);
+    }
+  });
   $("form#order").submit(function (event) {
     event.preventDefault();
+    var name = $("input#name").val();
+    var flavour = $("select#flavour").find(":selected").text();
+    var size = $("select#size").find(":selected").text();
+    var crust = $("select#crust").find(":selected").text();
+    var toppings = $('input[name="toppings"]:checked');
+    var number = $("input#number").val();
+    var delivery = $("input#delivery").val();
+    var location = $("textarea#location").val();
+
+    console.log("Name:" + name);
+    console.log("Flavour:" + flavour);
+    console.log("Size:" + size);
+    console.log("Crust:" + crust);
+    console.log("Toppings:" + toppings.serialize());
+    console.log("Number:" + number);
+    console.log("Delivery:" + delivery);
+    console.log("Location:" + location);
   });
 
   $("form#contact").submit(function (event) {
